4ba2c419291bcdb914130c8f3655a444508d5d59
Francisco <franciscomarcelo4001@gmail.com>
2024-10-06 17:05:31 +0100

    Gradle project.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
         double median = descriptiveStatistics.getPercentile(50);
         double standardDeviation = descriptiveStatistics.getStandardDeviation();
 
-        System.out.println("ola");
-        System.out.println("k");
+        //System.out.println("ola");
+        //System.out.println("k");
     }
 }
